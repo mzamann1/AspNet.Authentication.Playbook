8ab9e5dfba9d6c7cd4f145b2a17759a6766a084b
Muhammad Zaman <mzamann1@users.noreply.github.com>
2022-09-29 20:37:08 +0500

    setting up base project for differennt moduls.

--- Pluralsight.AspNetCore.Auth.Web/Program.cs
+++ Pluralsight.AspNetCore.Auth.Web/Program.cs
@@ -1,37 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Pluralsight.AspNetCore.Auth.Web.Services;
-
-
-var users = new Dictionary<string, string> { { "chris", "password" } };
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddMvc();
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-}).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, option =>
-{
-    option.LoginPath = "/auth/sign-in";
-    option.ReturnUrlParameter = "returnUrl";
-});
+var app= builder.Build();
 
-
-
-builder.Services.AddSingleton<IUserService>(new DummyUserService(users));
-
-
-var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
@@ -42,11 +20,6 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseMvcWithDefaultRoute();
 
 app.Run();
