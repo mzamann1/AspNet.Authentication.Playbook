cd998b10377787ecbe7813ea3e8344838ac8095b
Muhammad Zaman <mzamann1@users.noreply.github.com>
2022-09-29 22:29:12 +0500

    Facebook authentication added.

---
+++ Pluralsight.AspNetCore.Auth.Web/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pluralsight.AspNetCore.Auth.Web.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("auth")]
+    public class AuthController : Controller
+    {
+        [Route("sign-in")]
+        public IActionResult SignIn()
+        {
+            return Challenge(new AuthenticationProperties()
+            {
+                RedirectUri = "/"
+            });
+        }
+
+        [Route("sign-out")]
+        [HttpPost]
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Route("access-denied")]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+    }
+}
--- Pluralsight.AspNetCore.Auth.Web/Program.cs
+++ Pluralsight.AspNetCore.Auth.Web/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,7 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMvc();
 
-var app= builder.Build();
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = FacebookDefaults.AuthenticationScheme;
+    option.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+}).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, option =>
+    {
+
+    }).AddFacebook(fbOptions =>
+    {
+        fbOptions.AppId = builder.Configuration["Authentication:Facebook:AppId"]; //from secret manager
+        fbOptions.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"]; //from secret manager
+        fbOptions.AccessDeniedPath = "/auth/access-denied";
+    });
+
+var app = builder.Build();
 
 
 if (app.Environment.IsDevelopment())
@@ -20,6 +37,11 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
-app.UseMvcWithDefaultRoute();
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
