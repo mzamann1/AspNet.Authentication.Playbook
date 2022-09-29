b64656eca400241b885aa366c84bcc4213543678
Muhammad Zaman <mzamann1@users.noreply.github.com>
2022-09-30 02:16:21 +0500

    added Azure AD authentication and signout functionality

---
+++ Pluralsight.AspNetCore.Auth.Web/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pluralsight.AspNetCore.Auth.Web.Controllers;
+
+[Route("auth")]
+public class AuthController : Controller
+{
+    [Route("sign-in")]
+    public IActionResult SignIn()
+    {
+        return Challenge(new AuthenticationProperties()
+        {
+            RedirectUri = "/home"
+        });
+    }
+
+    [HttpPost, Route("sign-out")]
+    public async Task Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme).ConfigureAwait(false);
+        
+
+    }
+}
--- Pluralsight.AspNetCore.Auth.Web/Program.cs
+++ Pluralsight.AspNetCore.Auth.Web/Program.cs
@@ -1,14 +1,37 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddMvc();
+builder.Services.AddMvc(options => options.EnableEndpointRouting = true);
 
-var app= builder.Build();
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    })
+    .AddOpenIdConnect(options =>
+    {
+        options.Authority = "https://login.microsoftonline.com/net6playbook.onmicrosoft.com";
+        options.ClientId = "6c3a80f1-7669-44df-84cb-0ddd8b6d5e7f";
+        options.ResponseType = OpenIdConnectResponseType.IdToken;
+        options.CallbackPath = "/oauth/signin-callback";
+        options.SignedOutRedirectUri = "https://localhost:44343/home";
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            NameClaimType = "name"
+        };
+    }).AddCookie();
+
+var app = builder.Build();
 
 
 if (app.Environment.IsDevelopment())
@@ -20,6 +43,10 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
-app.UseMvcWithDefaultRoute();
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
 
 app.Run();
